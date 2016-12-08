8f5bebc82cb9e55aabd2889c6fe56fc22ef61246
ArthurDuytschaever <arthur.duytschaever@isen.yncrea.fr>
2016-12-08 09:51:50 +0100

    MAJ Test unitaire driver moteur integration avec Arduino Due

--- Asservissement/TestDriver/TestDriver1.0/TestDriver1.0.ino
+++ Asservissement/TestDriver/TestDriver1.0/TestDriver1.0.ino
@@ -1,29 +1,29 @@
-const int PWM = 3;
-const int IN1 = 4;
-const int IN2 = 5;
+ int pwm1 =3; 
+ int IN1 = 4;
+ int IN2 = 5;
 void setup() {
-  pinMode(PWM,OUTPUT);
+  pinMode(3,OUTPUT);
   pinMode(IN1,OUTPUT);
   pinMode(IN2,OUTPUT);
-
+  analogWriteResolution(8);// seulement sur Arduino Due 
 }
 
 void loop() {
   digitalWrite(IN1,LOW);
   digitalWrite(IN2,LOW);
-  analogWrite(PWM,1);
+  analogWrite(pwm1,1000);
   delay(5000);
   digitalWrite(IN1,HIGH);
   digitalWrite(IN2,LOW);
-  analogWrite(PWM,100);
+  analogWrite(3,100);
   delay(10000);
   digitalWrite(IN1,LOW);
   digitalWrite(IN2,LOW);
-  analogWrite(PWM,1);
+  analogWrite(3,10);
   delay(5000);
   digitalWrite(IN1,LOW);
   digitalWrite(IN2,HIGH);
-  analogWrite(PWM,1);
+  analogWrite(3,10);
   delay(10000);
 
 }
