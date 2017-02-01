5ffa500b6080a30f27ccd456b5b9eeb2e5ad3a5f
nsobczak <nicolas.sobczak@orange.fr>
2017-02-01 17:44:40 +0100

    Asservissement vitesse: asservissement croisé, moyenne des codeuses Ajout d'un début de tableur pour effectuer la courbe du PID

--- Module Moteur/Software/Asservissement/Asservissement vitesse/src/application.ino
+++ Module Moteur/Software/Asservissement/Asservissement vitesse/src/application.ino
@@ -19,35 +19,38 @@
  * ======================================================================================================
  */
 
-#define _DEBUG false
+#define _DEBUG true
 #define encoder0PinA_L 13   //encodeur gauche A
 #define encoder0PinB_L 12   //encodeur gauche B
 #define encoder0PinA_R 11   //encodeur droit A
 #define encoder0PinB_R 10   //encodeur droit B
 
-int MotorR =3; // Attention sur Due PWM ou Pwm sont des keyword -> donc ne pas les utiliser pour des nom de variable
-int MotorL = 6;
-int IN1MotorL = 22;
-int IN2MotorL = 23;
-int IN1MotorR = 52;
-int IN2MotorR = 53;
+#define MotorR 3 // Attention sur Due PWM ou Pwm sont des keyword -> donc ne pas les utiliser pour des nom de variable
+#define MotorL 6
+#define IN1MotorL 22
+#define IN2MotorL 23
+#define IN1MotorR 52
+#define IN2MotorR 53
 
 SimpleTimer timer;                 // Timer pour échantillonnage
-const int _MOTEUR =  9;            // Digital pin pour commande moteur
-unsigned int tick_codeuse = 0;     // Compteur de tick de la codeuse
+unsigned int tick_codeuse_R = 0;   // Compteur de tick de la codeuse
+unsigned int tick_codeuse_L = 0;   // Compteur de tick de la codeuse
+unsigned int tick_codeuse_moyenne = 0;   // Compteur de tick de la codeuse
 int cmd = 0;                       // Commande du moteur
 
 const int frequence_echantillonnage = 50;  // Fréquence du pid
-const int rapport_reducteur = 29;          // Rapport entre le nombre de tours de l'arbre moteur et de la roue
-const int tick_par_tour_codeuse = 32;      // Nombre de tick codeuse par tour de l'arbre moteur
+const int tick_par_tour_codeuse = 2500;      // Nombre de tick codeuse par tour de roue codeuse
+const int tick_par_tour_non_codeuse = 3836;      // Nombre de tick codeuse par tour de roue non codeuse
+const float rapport_roueCodeuse_roueNonCodeuse = (52.28)/(80.22);
 
-float consigne_moteur_nombre_tours_par_seconde = 5.;  //  Nombre de tours de roue par seconde
+
+float consigne_moteur_nombre_tours_par_seconde = 7.0;  //  Nombre de tours de roue par seconde
 
 float erreur_precedente = consigne_moteur_nombre_tours_par_seconde;
 float somme_erreur = 0;   // Somme des erreurs pour l'intégrateur
-float kp = 300;           // Coefficient proportionnel
-float ki = 5.5;           // Coefficient intégrateur
-float kd = 100;           // Coefficient dérivateur
+float kp = 0;           // Coefficient proportionnel
+float ki = 0;           // Coefficient intégrateur
+float kd = 0;           // Coefficient dérivateur
 
 
 /* ======================================================================================================
@@ -74,13 +77,18 @@ void setup()
     pinMode(encoder0PinA_R, INPUT);
     pinMode(encoder0PinB_R, INPUT);
 
-    pinMode(_MOTEUR, OUTPUT);     // Sortie moteur
-    analogWrite(_MOTEUR, 255);    // Sortie moteur à 0
+    //Moteur droit
+    digitalWrite(IN1MotorR, LOW);
+    digitalWrite(IN2MotorR, HIGH);
+    //Moteur gauche
+    digitalWrite(IN1MotorL, HIGH);
+    digitalWrite(IN2MotorL, LOW);
 
     delay(5000);                  // Pause de 5 sec pour laisser le temps au moteur de s'arréter si celui-ci est en marche
 
-    attachInterrupt(0, compteur, CHANGE);    // Interruption sur tick de la codeuse (interruption 0 = pin2 arduino mega)
-    timer.setInterval(1000/frequence_echantillonnage, asservissement;  // Interruption pour calcul du PID et asservissement
+    attachInterrupt(encoder0PinA_R, compteur_tick_R, CHANGE);    // Interruption sur tick de la codeuse (interruption 0 = pin2 arduino mega)
+    attachInterrupt(encoder0PinA_L, compteur_tick_L, CHANGE);    // Interruption sur tick de la codeuse (interruption 0 = pin2 arduino mega)
+    timer.setInterval(1000/frequence_echantillonnage, asservissement);  // Interruption pour calcul du PID et asservissement
 }
 
 
@@ -95,11 +103,20 @@ void setup()
 
 
 /**
- * \fn void compteur()
- * \brief Interruption sur tick de la codeuse
+ * \fn void compteur_tick_R()
+ * \brief Interruption sur tick de la codeuse right
  */
-void compteur(){
-    tick_codeuse++;  // On incrémente le nombre de tick de la codeuse
+void compteur_tick_R(){
+    tick_codeuse_R++;  // On incrémente le nombre de tick de la codeuse
+}
+
+
+/**
+ * \fn void compteur_tick_L()
+ * \brief Interruption sur tick de la codeuse right
+ */
+void compteur_tick_L(){
+    tick_codeuse_L++;  // On incrémente le nombre de tick de la codeuse
 }
 
 
@@ -110,12 +127,14 @@ void compteur(){
 void asservissement()
 {
    // Réinitialisation du nombre de tick de la codeuse
-   int tick = tick_codeuse;
-   tick_codeuse=0;
+   tick_codeuse_moyenne = (tick_codeuse_R + tick_codeuse_L) / 2;
+   int tick = tick_codeuse_moyenne;
+   tick_codeuse_R = 0;
+   tick_codeuse_L = 0;
 
    // Calcul des erreurs
    int frequence_codeuse = frequence_echantillonnage*tick;
-   float nb_tour_par_sec = (float)frequence_codeuse/(float)tick_par_tour_codeuse/(float)rapport_reducteur;
+   float nb_tour_par_sec = (float)frequence_codeuse/(float)tick_par_tour_non_codeuse/rapport_roueCodeuse_roueNonCodeuse;
    float erreur = consigne_moteur_nombre_tours_par_seconde - nb_tour_par_sec;
    somme_erreur += erreur;
    float delta_erreur = erreur-erreur_precedente;
@@ -125,13 +144,24 @@ void asservissement()
    cmd = kp*erreur + ki*somme_erreur + kd*delta_erreur;
 
    // Normalisation et contrôle du moteur
-   if(cmd < 0) cmd=0;
-   else if(cmd > 255) cmd = 255;
-   analogWrite(_MOTEUR, 255-cmd);
+  //  if(cmd < 0) cmd=255;
+  //  else if(cmd > 255) cmd = 0;
+  //  analogWrite(MotorR, (-1)*(cmd-255));
+  //  analogWrite(MotorL, (-1)*(cmd-255));
+  analogWrite(MotorR, 255);
+  analogWrite(MotorL, 255);
 
-   // DEBUG
-   if(_DEBUG)
-   {
-     Serial.println(nb_tour_par_sec, 8);
-   }
+  // DEBUG
+  if(_DEBUG)
+  {
+    Serial.print("\t cmd :\t");
+    Serial.println(cmd, 8);
+
+    Serial.print("\t tick_codeuse_moyenne  :\t");
+    Serial.println(tick_codeuse_moyenne, 8);
+
+    Serial.print("\t nb_tour_par_sec : \t");
+    Serial.println(nb_tour_par_sec, 8);
+  }
+
 }
