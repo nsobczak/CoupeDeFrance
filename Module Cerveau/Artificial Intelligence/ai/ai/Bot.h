63723040861378a6874b1fea683f6cc9c9ee7650
nsobczak <nicolas.sobczak@orange.fr>
2017-04-08 21:04:48 +0200

    Cerveau: AI - update cylinders lists

--- Module Cerveau/Artificial Intelligence/ai/ai/Bot.h
+++ Module Cerveau/Artificial Intelligence/ai/ai/Bot.h
@@ -35,8 +35,15 @@
 
 #define _NUMBER_OF_CYLINDERS_TO_CATCH_ 10 //TODO: replace by the right number of cylinders
 
-const CylinderPosition cylinderToCatchList_1B[_NUMBER_OF_CYLINDERS_TO_CATCH_]={{1,1,1},{1,1,1},{1,1,1},{1,1,1},{1,1,1},{1,1,1},{1,1,1},{1,1,1},{1,1,1},{1,1,1}};
-const CylinderPosition cylinderToCatchList_2Y[_NUMBER_OF_CYLINDERS_TO_CATCH_]={{1,1,1},{1,1,1},{1,1,1},{1,1,1},{1,1,1},{1,1,1},{1,1,1},{1,1,1},{1,1,1},{1,1,1}};
+//TODO: replace by the right positions of cylinders
+const CylinderPosition cylinderToCatchList_1B[_NUMBER_OF_CYLINDERS_TO_CATCH_]={
+        {1, 1.3, 124.34}, {2, 1, 1}, {3, 1, 1}, {4, 1, 1}, {5, 1, 1},
+        {6, 1, 1}, {7, 1, 1},{8, 1, 1}, {9, 1, 1}, {10, 1, 1}
+};
+const CylinderPosition cylinderToCatchList_2Y[_NUMBER_OF_CYLINDERS_TO_CATCH_]={
+        {1, 1.3, 124.34}, {2, 1, 1}, {3, 1, 1}, {4, 1, 1}, {5, 1, 1},
+        {6, 1, 1}, {7, 1, 1},{8, 1, 1}, {9, 1, 1}, {10, 1, 1}
+};
 
 
 /* ======================================================================================================
