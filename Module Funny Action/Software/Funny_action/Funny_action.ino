92f71a96a9b3b71b9c8d75e321b8fa201211c8be
JombartOlivier <olivier.jombart@isen.yncrea.fr>
2017-04-17 15:45:33 +0200

    Funny action : code final, en attente de test

--- Module Funny Action/Software/Funny_action/Funny_action.ino
+++ Module Funny Action/Software/Funny_action/Funny_action.ino
@@ -1,54 +1,72 @@
 #include <Servo.h>
+#include <Arduino.h>
 
-unsigned long previousMillis=0;
-unsigned long interval=3200L;
-const int PIN_D2=2;
-const int PIN_D6=6;
-const int PIN_D10=10;
-bool flag=false;
+unsigned long interval=92000L; //Au bout de 90s, lancement funny action
+const int PIN_D2=2; //Pin pour le bouton arret d'urgence, arret funny action si = HIGH
+const int PIN_D3=3; //Pin pour la lancé de la funny action, ordre venant de la carte mère : HIGH
+const int PIN_D5=5; // Pin test sortant un état HIGH
+const int PIN_D6=6; // Pin test sortant un état HIGH
+
+bool flag=false;    //arret de la funny une fois lancée
 
 Servo servo;
-void boutonArretUrgence(){
-  Serial.println("Arret funny action");
-  
-  
-}
 
-void servoMoteur(){
-  servo.write(60);
-  Serial.println("Fusee lancee");
+// permet le reset de la millis
+struct myMillis {
+  unsigned long offset = 0;
+  void set(unsigned long current) {
+    offset = millis() - current;
+  }
+  void reset() {
+    offset = millis();
+  }
+  unsigned long get() {
+    return millis() - offset;
+  }
+};
+
+myMillis MM;
+
+
+void  servoMoteur()
+{
+      servo.write(60);  // angle à corriger
+      Serial.println("Fusee lancee");
 }
 
 void setup() {
-  // put your setup code here, to run once:
-  attachInterrupt(digitalPinToInterrupt(PIN_D2),boutonArretUrgence,HIGH);
-  pinMode(PIN_D6,INPUT);
-  pinMode(PIN_D10,OUTPUT);
-  pinMode(7,OUTPUT);
-  pinMode(3,OUTPUT);
-  Serial.begin(9600);
-  servo.attach(PIN_D2);
   
+      pinMode(PIN_D2,INPUT);
+      pinMode(PIN_D5,OUTPUT);
+      pinMode(PIN_D6,OUTPUT);
+      attachInterrupt(digitalPinToInterrupt(PIN_D3),interruption,HIGH);   
+      Serial.begin(9600);
+      servo.attach(10);
+      digitalWrite(PIN_D5,HIGH);
+      digitalWrite(PIN_D6,HIGH);
+      
 }
 
+/**
+ * fonction d'interruption à la pin D2, reset la milis au lancement de l'ordre
+ */
+void interruption() {
+      MM.reset();
+      Serial.println("reset millis");
+}
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("0");
-  digitalWrite(7,HIGH);
-  if (digitalRead(PIN_D6)==HIGH&&flag==false) {
-    if (previousMillis<=interval){
-      previousMillis=millis();
-      Serial.print("previousMillis = "); 
-      Serial.print(previousMillis);
-      delay(100);
-      Serial.println("\nmillis = ");
-      Serial.print(millis());
-    
-          
-  }
-  servoMoteur();
-  flag=true;
-  }
   
+      if (digitalRead(PIN_D3)==HIGH&&flag==false&&digitalRead(PIN_D2)==LOW){ 
+        
+            Serial.println(MM.get());
+            if(MM.get()>=interval){
+                    servoMoteur();  
+                    flag=true;     
+            }
+  }
+   
   
+
 }
